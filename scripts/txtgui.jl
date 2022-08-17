278574dac80144f866037ea4a0393fdc93c2da86
Sukhoverkhaya <90982979+Sukhoverkhaya@users.noreply.github.com>
2022-08-17 19:23:20 +0300

    .

--- scripts/txtgui.jl
+++ scripts/txtgui.jl
@@ -1,4 +1,4 @@
-module ConclusionGui
+# module ConclusionGui
     using CImGui
     using ImPlot
     using JSON
@@ -14,15 +14,19 @@ module ConclusionGui
     include("../src/parsetxt.jl")
     using .parsetxt
 
+    struct Node
+        name::String
+        ban::Vector{String}
+    end
+
     mutable struct Global
         filename::String
         data::Vector{Any}
         is_file_loaded::Bool
         current_item::Vector{String}
         is_group_selected::Bool
-        conclusion::String
-        isfin::Bool
         final::String
+        newphrase::String
 
         function Global()
             filename = ""
@@ -30,46 +34,55 @@ module ConclusionGui
             is_file_loaded = false
             current_item = []
             is_group_selected = false
-            conclusion = "nothing"
-            isfin = false
             final = ""
+            newphrase = ""
 
-            new(filename, data, is_file_loaded, current_item, is_group_selected, conclusion, isfin, final)
+            new(filename, data, is_file_loaded, current_item, is_group_selected, final, newphrase)
         end
     end
 
-    function ui(v::Global)
-        function makeconclusion()
-            conclusion = ""
-            for i in 1:length(v.current_item)
-                if v.current_item[i] != ""
-                    conclusion *= v.current_item[i]*". "
-                end
-            end
+    function foo(full::String)
+        @cstatic t="" begin
+        t = full*"\0"^(10000-length(full))
+        end
+    end
 
-            txt = split(conclusion, "")
-            full = "    "
-            k = 0
-            for i in 1:length(txt)
-                if k < 150
-                    full *= txt[i]
-                    k += 1
-                else
-                    if txt[i] == " "
-                        full *= "\n"
-                        full *= txt[i]
-                        k = 0
-                    else
-                        full *= txt[i]
-                        k += 1
-                    end
-                end
+    function makeconclusion(v::Global)
+        conclusion = ""
+        for i in 1:length(v.current_item)
+            if v.current_item[i] != ""
+                conclusion *= v.current_item[i]*". "
             end
-            v.conclusion = full
         end
 
+        txt = split(conclusion, "")
+        full = "    "
+        k = 0
+        for i in 1:length(txt)
+            if k < 150
+                full *= txt[i]
+                k += 1
+            else
+                if txt[i] == " "
+                    full *= "\n"
+                    full *= txt[i]
+                    k = 0
+                else
+                    full *= txt[i]
+                    k += 1
+                end
+            end
+        end
+
+        final = foo(full)
+
+        return final
+    end
+
+    function ui(v::Global)
+
         CImGui.SetNextWindowPos(ImVec2(0,0))
-        CImGui.SetNextWindowSize(ImVec2(2500,760))
+        CImGui.SetNextWindowSize(ImVec2(2100,1000))
         CImGui.Begin("Меню")
             if CImGui.Button("Загрузить файл")
                 v.filename = open_dialog_native("Выберите файл", GtkNullContainer(), ("*.txt",))
@@ -78,27 +91,24 @@ module ConclusionGui
                     v.is_file_loaded = false
                 else
                     v.data = parsetxt.my_txtparser(v.filename)
-
                     v.current_item = fill("", length(v.data[2])+1)
                     v.is_file_loaded = true
                 end
             end
 
             if v.is_file_loaded
+                CImGui.SameLine(1664)
+                if CImGui.Button("Очистить выбор (новое заключение)")
+                    v.current_item = fill("", length(v.data[2])+1)
+                    v.is_group_selected = false
+                    v.final = ""
+                end
                 if CImGui.BeginCombo("Группы ритмов", v.current_item[1])
                     for i in 1:length(v.data[1])
                         isselected = (v.data[1][i].name == v.current_item[1])
                         if CImGui.Selectable(v.data[1][i].name, isselected)
                             v.current_item[1] = v.data[1][i].name
                             v.is_group_selected = true
-
-                            makeconclusion()
-                            function foo()
-                                @cstatic t="" begin
-                                t = v.conclusion*"\0"^10000
-                                end
-                            end
-                            v.final = foo()
                             
                             for j in 2:length(v.current_item)
                                 needclean = false
@@ -111,6 +121,7 @@ module ConclusionGui
                                     v.current_item[j] = ""
                                 end
                             end
+                            v.final = makeconclusion(v)
                         end
                         if isselected
                             CImGui.SetItemDefaultFocus()
@@ -118,6 +129,7 @@ module ConclusionGui
                     end
                     CImGui.EndCombo()
                 end
+
             end
 
             if v.is_group_selected
@@ -134,15 +146,7 @@ module ConclusionGui
                             if !is_banned
                                 if CImGui.Selectable(v.data[2][i].children[j].name, isselected)
                                     v.current_item[i+1] = v.data[2][i].children[j].name
-                                    
-                                    makeconclusion()
-                                    function foo()
-                                        @cstatic t="" begin
-                                        t = v.conclusion*"\0"^10000
-                                        end
-                                    end
-                                    v.final = foo()
-
+                                    v.final = makeconclusion(v)
                                 end
                                 if isselected
                                     CImGui.SetItemDefaultFocus()
@@ -151,16 +155,83 @@ module ConclusionGui
                         end
                         CImGui.EndCombo()
                     end
+                    if v.current_item[i+1] != ""
+                        CImGui.PushID(v.data[2][i].name)
+                            CImGui.SameLine()
+                            if CImGui.SmallButton("очистить поле")
+                                v.current_item[i+1] = ""
+                                v.final = makeconclusion(v)
+                            end
+                        CImGui.PopID()
+                    end
                 end
             end
+
+            if v.is_file_loaded
+                CImGui.Separator()
+                if CImGui.TreeNode("Поле добавления пользовательских фраз")
+                    @cstatic phrase="\0"^500 begin
+                        @cstatic selection = fill(false, 9) begin
+                        CImGui.Text("Введите новую фразу")
+                        CImGui.InputText("##1", phrase, length(phrase))
+
+                        CImGui.SameLine()
+                        if CImGui.Button("Добавить фразу")
+                            open(v.filename, "a+") do io
+                                write(io, "\n"*"    "*replace(v.newphrase, "\0" => ""))
+                            end   
+                            phrase = "\0"^500
+                            selection = fill(false, 9)
+                            warn_dialog("Новая фраза добавлена в раздел `Пользовательские фразы'.")
+
+                            v.data = parsetxt.my_txtparser(v.filename)
+                        end
+
+                        if CImGui.TreeNode("Выберите группы ритмов, для которых фраза будет запрещена:")
+                                window_flags = CImGui.ImGuiWindowFlags_HorizontalScrollbar
+                                CImGui.BeginChild("Child1", ImVec2(CImGui.GetWindowContentRegionWidth() * 0.5, 260), false, window_flags)
+                                    for i in 1:length(v.data[1])
+                                        CImGui.Selectable(v.data[1][i].name, pointer(selection)+(i-1)*sizeof(Bool))
+                                    end
+                                CImGui.EndChild()
+
+                                code = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+                                for i in 1:length(selection)
+                                    if !selection[i]
+                                        code[i] = "remove"
+                                    end
+                                end
+                                filter!(e->e≠"remove", code)
+                                if length(code) == 0
+                                    ban = ""
+                                elseif length(code) == 1
+                                    ban = code[1]
+                                else
+                                    ban = code[1]
+                                    for i in 2:length(code)
+                                        ban *= ", "*code[i]
+                                    end
+                                end
+                                if ban == ""
+                                    v.newphrase = phrase
+                                else
+                                    v.newphrase = phrase*" : "*ban
+                                end
+                            end
+                        end
+
+                    end
+                end
+            end
+
         CImGui.End()
 
-        CImGui.SetNextWindowPos(ImVec2(0, 765))
-        CImGui.SetNextWindowSize(ImVec2(2500,650))
+        CImGui.SetNextWindowPos(ImVec2(0, 1005))
+        CImGui.SetNextWindowSize(ImVec2(2100,650))
         CImGui.Begin("Заключение")
             CImGui.BulletText("Поле с заключением доступно для редактирования.")
             CImGui.BulletText("Если после редактирования заключения выбор фраз будет изменён, результаты корректировки вручную будут утеряны.")
-            CImGui.SameLine(2100)
+            CImGui.SameLine(1816)
             if CImGui.Button("Сохранить заключение")
                 fname=save_dialog_native("Select file", GtkNullContainer(), ("*.txt",))
                 write(fname, replace(v.final, "\0" => "" ))
@@ -176,13 +247,11 @@ module ConclusionGui
         state = Global();
         Renderer.render(
             ()->ui(state),
-            width=2500,
-            height=1400,
-            title="",
-            hotloading=true
+            width=2100,
+            height=1620,
+            title=""
         )
-        return state
     end
 
-    # show_gui();
-end
+    show_gui();
+# end
