47f85a9e7840dace35224f58a39eb6384c1cfd06
Sukhoverkhaya <90982979+Sukhoverkhaya@users.noreply.github.com>
2022-08-24 17:34:32 +0300

    .

--- scripts/compile.jl
+++ scripts/compile.jl
@@ -1,8 +1,9 @@
 using PackageCompiler
 
-include("txtgui.jl")
-using .ConclusionGui
+# PackageCompiler.create_app(pwd(), "MyProjectCompiled";
+#     cpu_target="generic;sandybridge,-xsaveopt,clone_all;haswell,-rdrnd,base(1)",
+#     include_transitive_dependencies=false,
+#     filter_stdlibs=true,
+#     precompile_execution_file=["scripts/newgui.jl"])
 
-ConclusionGui.show_gui()
-
-create_app("../Project.toml", "txtgui.jl")
+PackageCompiler.create_app(pwd(), "HotBoxCompiled"; filter_stdlibs=true, precompile_execution_file=["scripts/newgui.jl"])
